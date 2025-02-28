e59732e5e6b114e693db2d26b776972cebd520bd
Wesley <wesleysant909@gmail.com>
2025-02-28 11:14:45 -0300

    Estudos

---
+++ javascript/js07/js/script.js
@@ -0,0 +1,10 @@
+
+
+/*
+    Abaixo de 17 Muito abaixo do peso
+    Entre 17 e 18,49 Abaixo do peso
+    Entre 18,5 e 24,99 Peso normal
+    Ebtre 25 e 29,99 Acima do peso
+*/
+
+// peso /(altura*altura);
